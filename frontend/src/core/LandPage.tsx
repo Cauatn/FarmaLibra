78db3d7c9b111ef73ca20cd3d6ed2a3260e49c22
cauatn <tavarescauac@gmail.com>
2024-10-06 21:10:44 -0300

    style: new design for LandPage

--- frontend/src/core/LandPage.tsx
+++ frontend/src/core/LandPage.tsx
@@ -1,68 +1,187 @@
 import { Button } from "@/components/ui/button";
-import Univasf from "@/assets/univasf.png";
-import cim from "@/assets/cim.png";
-import elipse from "@/assets/elipse_tela_de_inicio.png";
-import cff from "@/assets/conselho-federal-de-farmacia.png";
+import { ShieldCloseIcon } from "lucide-react";
+import { useState } from "react";
+import StaticLogoCloud from "@/components/Logos";
 import { useNavigate } from "react-router-dom";
+
+const navigation = {
+  connect: [
+    {
+      name: "Instagram",
+      href: "https://instagram.com/cauatn",
+    },
+    {
+      name: "Github",
+      href: "https://www.github.com/cauatn/Farmalibras",
+    },
+  ],
+  company: [
+    { name: "Conselho Federal de Libras", href: "/" },
+    { name: "Univasf", href: "/" },
+    { name: "Cim", href: "/" },
+  ],
+};
 
 function LandPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="relative mx-auto max-w-7xl pt-20">
-      <img
-        src={elipse}
-        alt="Elipse"
-        className="absolute -top-5 left-0 -z-10 h-auto w-full"
-      />
-      <main className="flex h-full min-h-fit w-full items-start justify-between space-x-3">
-        <div className="flex h-full flex-grow flex-col justify-between">
-          <div className="flex min-h-[435px] flex-col space-y-8">
-            <span className="whitespace-nowrap text-5xl font-semibold">
-              BEM-VINDO AO FARMALIBRA
-            </span>
-            <p className="text-justify text-2xl">
-              Idealizado por estudantes de graduação em Farmácia pela
-              Universidade Federal do Vale do São Francisco como uma ferramenta
-              que irá mediar a comunicação entre pessoas ouvintes e surdas no
-              atendimento clínico farmacêutico com o objetivo de gerar
-              igualdade, acessibilidade, e promover o uso racional de
-              medicamentos.
-            </p>
-          </div>
-          <div className="mt-auto flex justify-between space-x-4">
-            <Button className="max-h-[75px] w-full max-w-[340px] bg-[#FFCD00] p-8 text-xl font-semibold text-black">
-              Mais informações
-            </Button>
-            <Button
-              className="max-h-[75px] w-full max-w-[340px] bg-[#FFCD00] p-8 text-xl font-semibold text-black"
-              onClick={() => navigate("/inicio")}
-            >
-              Iniciar
-            </Button>
-          </div>
-        </div>
-
-        <div className="h-[500px] w-[10px] bg-gray-400"></div>
-
-        <div className="flex flex-col items-center">
-          <iframe
-            width="500"
-            height="400"
-            src="https://www.youtube.com/embed/OscyJ0bt6jM"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-            allowFullScreen
-            className="h-[400px] w-[600px]"
-          ></iframe>
-          <div className="mt-4 flex w-full justify-between">
-            <img src={Univasf} alt="Univasf Logo" className="h-20 w-40" />
-            <img src={cim} alt="CIM Logo" className="h-20 w-40" />
-            <img src={cff} alt="CFF Logo" className="h-20 w-40" />
+    <div className="flex flex-col">
+      <main className="container mx-auto flex min-h-screen flex-grow flex-col justify-between">
+        <Header />
+        <div className="flex flex-grow items-center justify-between">
+          <div className="mt-14 flex flex-grow justify-between">
+            <div className="h-full w-[40%] pr-8">
+              <h1 className="mb-4 text-start text-6xl font-semibold">
+                Bem Vindo ao Farmalibra
+              </h1>
+              <p className="mb-8 text-start text-xl text-gray-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                popularised i
+              </p>
+              <div className="inline-flex w-full justify-between space-x-4">
+                <Button className="h-11 rounded-md bg-gray-900 px-8 py-5 text-white hover:bg-gray-800">
+                  Aprender Mais
+                </Button>
+                <Button
+                  className="h-11 rounded-md bg-red-500 px-8 py-5 text-white hover:bg-red-600"
+                  onClick={() => navigate("/inicio")}
+                >
+                  Comece já
+                </Button>
+              </div>
+            </div>
+            <iframe
+              width="500"
+              height="400"
+              src="https://www.youtube.com/embed/OscyJ0bt6jM?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&loop=1&playlist=OscyJ0bt6jM"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+              allowFullScreen
+              className="w-[50%]"
+            ></iframe>
           </div>
         </div>
+        <StaticLogoCloud />
       </main>
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <>
+      <div
+        // replace `absolute` with `fixed` if you want the banner to be fixed on the page Also Animation will not work if you use `absolute`
+        className={`absolute left-0 right-0 top-0 z-50 transition-all duration-300 ease-in-out ${
+          isVisible ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <div className="flex w-full items-center gap-x-6 bg-yellow-400 px-6 py-3 sm:px-3.5 sm:before:flex-1">
+          <div className="flex items-center text-sm font-medium leading-6 text-white">
+            <p>Lorem Ipsum is simply dummy text of the printing.</p>
+
+            <a className="ml-3 flex-none rounded-lg bg-gray-900 px-4 py-2 text-xs text-white shadow-sm hover:bg-gray-900/80">
+              Lorem Ipsum
+            </a>
+          </div>
+          <div className="flex flex-1 justify-end">
+            <button
+              type="button"
+              onClick={() => setIsVisible(false)}
+              aria-label="Dismiss"
+            >
+              <ShieldCloseIcon className="h-5 w-5 text-white" />
+            </button>
+          </div>
+        </div>
+      </div>
+      {isVisible && <div className="h-[52px]" />}
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <footer
+      aria-labelledby="footer-heading"
+      className="font-inter m-auto w-full max-w-7xl align-bottom"
+    >
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-2">
+        <div className="flex flex-col justify-between lg:flex-row">
+          <div className="space-y-8">
+            <img
+              width={100}
+              height={40}
+              src="/images/placeholder.svg"
+              alt="logo"
+              className="h-7 w-auto"
+            />
+            <p className="text-md max-w-xs leading-6 text-gray-700 dark:text-gray-300">
+              Not your average component library - build faster, launch sooner.
+            </p>
+            <div className="flex space-x-6 text-sm text-gray-700 dark:text-gray-300">
+              <div>Feito com ❤️ por INSERIR.</div>
+            </div>
+          </div>
+          {/* items */}
+          <div className="mt-16 grid grid-cols-2 gap-14 md:grid-cols-2 lg:mt-0 xl:col-span-2">
+            <div className="md:mt-0">
+              <h3 className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-200">
+                Fale com a gente
+              </h3>
+              <div className="mt-6 space-y-4">
+                {navigation.connect.map((item) => (
+                  <div key={item.name}>
+                    <a
+                      href={item.href}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-sm leading-6 text-gray-700 hover:text-gray-900 dark:text-gray-600 hover:dark:text-gray-200"
+                    >
+                      {item.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div>
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-200">
+                  Instituições
+                </h3>
+                <div className="mt-6 space-y-4">
+                  {navigation.company.map((item) => (
+                    <div key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-gray-700 hover:text-gray-900 dark:text-gray-600 hover:dark:text-gray-200"
+                      >
+                        {item.name}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 border-t border-gray-900/10 pt-8 dark:border-gray-100/10 sm:mt-20 lg:mt-6 lg:pb-2">
+          <p className="text-xs leading-5 text-gray-700 dark:text-gray-300">
+            &copy; 2024 Farmalibras. Todos os direitos reservados.
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 }
 
