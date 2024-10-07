3ef21bc54e45abdf56e741a2bb744f302d0d84da
cauatn <tavarescauac@gmail.com>
2024-10-06 21:10:58 -0300

    refactor: Improve Header component and add NavButton component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,65 +1,81 @@
 import { HelpCircle, Plane } from "lucide-react";
 import { ArrowLeftIcon, CircleIcon, HomeIcon, Share2Icon } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 
 function Header({ showBack, route }: { showBack?: boolean; route?: string }) {
   const navigate = useNavigate();
   return (
     <header className="relative z-10 bg-black p-2 text-white">
-      <nav className="flex w-full items-center justify-between">
-        {showBack && (
-          <Button
-            className="flex items-center space-x-1 text-sm"
-            onClick={() => {
-              if (route) navigate(route);
-              else navigate(-1);
-            }}
-          >
-            <ArrowLeftIcon className="h-4 w-4" />
-            <span>VOLTAR</span>
-          </Button>
-        )}
-        <div className="ml-40 flex w-full items-center justify-start space-x-20">
-          <Button
-            className="flex items-center space-x-1 text-sm"
-            onClick={() => navigate("/inicio")}
-          >
-            <HomeIcon className="h-4 w-4" />
-            <span>INÍCIO</span>
-          </Button>
-          <Button
-            className="rounded bg-yellow-400 px-2 py-1 text-sm font-bold text-black"
-            onClick={() => navigate("/chat")}
-          >
-            <CircleIcon className="mr-1 inline h-4 w-4" />
-            ATENDIMENTO
-          </Button>
-          <Button
-            className="flex items-center space-x-1 text-sm"
-            onClick={() => navigate("")}
-          >
-            <Share2Icon className="h-4 w-4" />
-            <span>ÁREA DE APRENDIZADO</span>
-          </Button>
-          <Button
-            className="flex items-center space-x-1 text-sm"
-            onClick={() => navigate("/sugestion")}
-          >
-            <Plane className="h-4 w-4" />
-            <span>SUGESTÕES</span>
-          </Button>
-          <Button
-            className="flex items-center space-x-1 text-sm"
-            onClick={() => navigate("")}
-          >
-            <HelpCircle className="h-4 w-4" />
-            <span>AJUDA</span>
-          </Button>
-        </div>
-      </nav>
+      <div className="mx-auto max-w-7xl">
+        <nav className="flex w-full items-center justify-between">
+          {showBack && (
+            <Button
+              className="flex items-center space-x-1 text-sm"
+              onClick={() => {
+                if (route) navigate(route);
+                else navigate(-1);
+              }}
+            >
+              <ArrowLeftIcon className="h-4 w-4" />
+              <span>VOLTAR</span>
+            </Button>
+          )}
+          <div className="ml-40 flex w-full items-center justify-start space-x-20">
+            <NavButton
+              label="INÍCIO"
+              icon={<HomeIcon className="h-4 w-4" />}
+              route="/inicio"
+            />
+            <NavButton
+              label="ATENDIMENTO"
+              icon={<CircleIcon className="mr-1 inline h-4 w-4" />}
+              route="/chat"
+            />
+            <NavButton
+              label="ÁREA DE APRENDIZADO"
+              icon={<Share2Icon className="h-4 w-4" />}
+              route="#"
+            />
+            <NavButton
+              label="SUGESTÕES"
+              icon={<Share2Icon className="h-4 w-4" />}
+              route="/sugestion"
+            />
+            <NavButton
+              label="AJUDA"
+              icon={<HelpCircle className="h-4 w-4" />}
+              route="/ajuda"
+            />
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
+
+interface NavButtonProps {
+  label: string;
+  icon: React.ReactNode;
+  route: string;
+}
+
+const NavButton: React.FC<NavButtonProps> = ({ label, icon, route }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isActive = location.pathname === route;
+
+  return (
+    <Button
+      className={`flex items-center space-x-1 text-sm hover:bg-yellow-500 ${
+        isActive ? "bg-yellow-400 text-black" : ""
+      }`}
+      onClick={() => navigate(route)}
+    >
+      {icon}
+      <span>{label}</span>
+    </Button>
+  );
+};
 
 export default Header;
