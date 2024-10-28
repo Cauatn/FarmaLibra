4bff90254017bd3baf207f6c5656f33b22e6135e
cauatn <tavarescauac@gmail.com>
2024-10-27 21:32:17 -0300

    style: Update CustomDialog to include last message for each type

--- frontend/src/components/CustomDialog.tsx
+++ frontend/src/components/CustomDialog.tsx
@@ -7,7 +7,7 @@ import {
 import cliente from "@/assets/imagem_cliente.png";
 import farmaceutico from "@/assets/imagem_farmaceutico_tr.png";
 import { useState } from "react";
-import { useActionStore } from "@/db/buffer";
+import { useActionStore, useListVideos } from "@/db/buffer";
 
 import DialogCustomContent from "./DialogCustomContent";
 
@@ -17,7 +17,14 @@ import DialogCustomContent from "./DialogCustomContent";
 function CustomDialog({ type }: { type: string }) {
   const { setAction } = useActionStore();
 
+  const { items } = useListVideos();
+
   const [open, setOpen] = useState(false);
+
+  const lastMessage =
+    items
+      .filter((item) => item.action.toLowerCase() === type.toLowerCase())
+      .slice(-1)[0]?.message || "";
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -29,25 +36,31 @@ function CustomDialog({ type }: { type: string }) {
         }}
       >
         {type == "Cliente" ? (
-          <div className="relative inline-flex h-14 w-[300px] items-center justify-between bg-white hover:cursor-pointer md:w-[280px]">
-            <div className="h-full w-2 bg-[#FFCD00]"></div>
-            <img
-              src={cliente}
-              alt=""
-              className="absolute -top-14 left-4 w-28"
-            />
+          <div className="flex flex-col items-center">
+            <div className="relative inline-flex h-14 w-[300px] items-center justify-between bg-white hover:cursor-pointer md:w-[280px]">
+              <div className="h-full w-2 bg-[#FFCD00]"></div>
+              <img
+                src={cliente}
+                alt=""
+                className="absolute -top-14 left-4 w-28"
+              />
 
-            <p className="mr-9 text-xl font-bold">{type}</p>
+              <p className="mr-9 text-xl font-bold">{type}</p>
+            </div>
+            <div className="mt-2 text-xl">{lastMessage}</div>
           </div>
         ) : (
-          <div className="relative inline-flex h-14 w-[300px] items-center bg-white hover:cursor-pointer md:w-[280px]">
-            <div className="h-full w-2 bg-[#FFCD00]"></div>
-            <p className="ml-2 text-xl font-bold">Farmacêutico(A)</p>
-            <img
-              src={farmaceutico}
-              alt=""
-              className="absolute -top-14 right-0 w-28"
-            />
+          <div className="flex flex-col items-center">
+            <div className="relative inline-flex h-14 w-[300px] items-center bg-white hover:cursor-pointer md:w-[280px]">
+              <div className="h-full w-2 bg-[#FFCD00]"></div>
+              <p className="ml-2 text-xl font-bold">Farmacêutico(A)</p>
+              <img
+                src={farmaceutico}
+                alt=""
+                className="absolute -top-14 right-0 w-28"
+              />
+            </div>
+            <div className="mt-2 text-xl">{lastMessage}</div>
           </div>
         )}
       </DialogTrigger>
